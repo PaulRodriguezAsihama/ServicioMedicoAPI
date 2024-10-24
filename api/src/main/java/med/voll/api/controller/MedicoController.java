43d98f1bfdf57a95f35a86e99c84d1b70f23e487
PaulRodriguezAsihama <pau.rodriguez.asihama@hotmail.com>
2024-10-24 15:27:02 -0400

    Despues de enviar Datos Medicos

--- api/src/main/java/med/voll/api/controller/MedicoController.java
+++ api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -2,14 +2,14 @@ package med.voll.api.controller;
 
 
 import jakarta.validation.Valid;
+import med.voll.api.medico.DatosListadoMedico;
 import med.voll.api.medico.DatosRegistroMedico;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/medicos")
@@ -23,4 +23,10 @@ public class MedicoController {
         medicoRepository.save(new Medico(datosRegistroMedico));
 
     }
+
+    @GetMapping
+    public List<DatosListadoMedico> listadoMedico(){
+        return medicoRepository.findAll().stream().map(DatosListadoMedico::new).toList();
+    }
+
 }
--- api/src/main/java/med/voll/api/paciente/PacienteController.java
+++ api/src/main/java/med/voll/api/paciente/PacienteController.java
@@ -1,6 +1,8 @@
 package med.voll.api.paciente;
 
 
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,10 +11,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("pacientes")
 public class PacienteController {
+    @Autowired
+    private PacienteRepository repository;
 
     @PostMapping
+    @Transactional
     public void registrar(@RequestBody DatosRegistroPaciente datos) {
-        System.out.println("datos recebidos: " +datos);
+        repository.save(new Paciente(datos));
     }
 
 }
