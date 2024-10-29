f8fab42105e35fb3d7f948705e3c11d3364deabc
PaulRodriguezAsihama <pau.rodriguez.asihama@hotmail.com>
2024-10-29 08:48:57 -0400

    Antes de la implementacion del login

---
+++ api/src/main/java/med/voll/api/infra/security/SecurityConfiguration.java
@@ -0,0 +1,23 @@
+package med.voll.api.infra.security;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.csrf(c -> c.disable())
+                .cors(c-> c.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth.requestMatchers("/login").permitAll().anyRequest().authenticated())
+                .build();
+    }
+}
